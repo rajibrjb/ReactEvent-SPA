0fdded73dfe9aec7114e133d9089e3cede4b9a3a
Rakib-RS <rakib.rscse@gmail.com>
2020-01-24 17:44:50 +0600

    folder structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 const rootEl = document.getElementById('root');
 let render = ()=>{
     ReactDOM.render(<App />,rootEl);
 }
 if(module.hot){
-    module.hot.accept('./App',()=>{
+    module.hot.accept("./app/layout/App",()=>{
 
         setTimeout(render);
     })
