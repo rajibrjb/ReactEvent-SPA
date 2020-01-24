d8d6cb312ae559213cbf40e27af4245425e2f88b
Rakib-RS <rakib.rscse@gmail.com>
2020-01-24 16:56:28 +0600

    Hot Module Replacement

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+const rootEl = document.getElementById('root');
+let render = ()=>{
+    ReactDOM.render(<App />,rootEl);
+}
+if(module.hot){
+    module.hot.accept('./App',()=>{
 
-ReactDOM.render(<App />, document.getElementById('root'));
+        setTimeout(render);
+    })
+}
+render();
+
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
