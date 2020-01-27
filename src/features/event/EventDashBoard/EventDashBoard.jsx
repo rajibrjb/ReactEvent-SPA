82d80e5c06345e9167c758bf1b2595abd064b2c2
Rakib-RS <rakib.rscse@gmail.com>
2020-01-27 23:44:26 +0600

    event list item added

--- src/features/event/EventDashBoard/EventDashBoard.jsx
+++ src/features/event/EventDashBoard/EventDashBoard.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { Grid } from 'semantic-ui-react'
+import EventList from '../EventList/EventList'
 
 export default class EventDashBoard extends Component {
     render() {
         return (
             <Grid>
                 <Grid.Column width={10}>
-                    Left Column
+                    <EventList/>
                 </Grid.Column>
                 <Grid.Column width={6}>
                     Right Column
---
+++ src/features/event/EventList/EventList.jsx
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react'
+
+class EventList extends Component {
+    render() {
+        return (
+            <Fragment>
+                
+            </Fragment>
+        )
+    }
+}
+export default  EventList
