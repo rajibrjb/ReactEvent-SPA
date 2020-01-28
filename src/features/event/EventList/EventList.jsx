38c2fb3b4afbf92c51df41e98f158f06bdc37580
Rakib-RS <rakib.rscse@gmail.com>
2020-01-28 12:25:30 +0600

    event list item added

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -1,10 +1,13 @@
 import React, { Component, Fragment } from 'react'
+import EventListItem from './EventListItem'
 
 class EventList extends Component {
     render() {
         return (
             <Fragment>
-                
+                <EventListItem/>
+                <EventListItem/>
+                <EventListItem/>
             </Fragment>
         )
     }
--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -1,38 +1,38 @@
 import React, { Component } from 'react'
-import { SegmentGroup, Segment, Item, Icon, List, Button } from 'semantic-ui-react'
+import { Segment, Item, Icon, List, Button } from 'semantic-ui-react'
 
 class EventListItem extends Component {
     render() {
         return (
-                <SegmentGroup.Group>
-                   <Segment>
-                     <Item.Group>
-                       <Item>
-                         <Item.Image size="tiny" circular src="" />
-                         <Item.Content>
-                           <Item.Header as="a">Event Title</Item.Header>
-                           <Item.Description>
-                             Hosted by <a>hosted by</a>
-                           </Item.Description>
-                         </Item.Content>
-                       </Item>
-                     </Item.Group>
-                   </Segment>
-                   <Segment>
-                     <span>
-                       <Icon name="clock" /> date |
-                       <Icon name="marker" /> time
-                     </span>
-                   </Segment>
-                   <Segment secondary>
-                     <List horizontal>
-                       {/* todo: attendees go here */}
-                     </List>
-                   </Segment>
-                   <Segment clearing>
-                     <Button as="a" color="teal" floated="right" content="View" />
-                   </Segment>
-                 </SegmentGroup.Group>
+                     <Segment.Group>
+                        <Segment>
+                          <Item.Group>
+                            <Item>
+                              <Item.Image size="tiny" circular src="" />
+                              <Item.Content>
+                                <Item.Header as="a">Event Title</Item.Header>
+                                <Item.Description>
+                                  Hosted by <a href="ok">hosted by</a>
+                                </Item.Description>
+                              </Item.Content>
+                            </Item>
+                          </Item.Group>
+                        </Segment>
+                        <Segment>
+                          <span>
+                            <Icon name="clock" /> date |
+                            <Icon name="marker" /> time
+                          </span>
+                        </Segment>
+                        <Segment secondary>
+                          <List horizontal>
+                            {/* todo: attendees go here */}
+                          </List>
+                        </Segment>
+                        <Segment clearing>
+                          <Button as="a" color="teal" floated="right" content="View" />
+                        </Segment>
+                      </Segment.Group>
         )
     }
 }
