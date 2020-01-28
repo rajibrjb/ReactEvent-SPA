a1ec77655b6c592baf4c92ace76f8bb87574b48e
Rakib-RS <rakib.rscse@gmail.com>
2020-01-28 15:27:36 +0600

    Passing down static props to components

--- src/features/event/EventList/EventListAttendee.jsx
+++ src/features/event/EventList/EventListAttendee.jsx
@@ -3,10 +3,11 @@ import { List, Image } from 'semantic-ui-react'
 
 class EventListAttendee extends Component {
     render() {
+        const {attendee} = this.props;
         return (
             <List.Item>
-                <Image as="a" size="mini"circular src="https://randomuser.me/api/portraits/women/42.jpg" />
-                <Image as="a" size="mini" circular src="https://avatars2.githubusercontent.com/u/37169080?s=400&v=4"></Image>
+                <Image as="a" size="mini"circular src={attendee.photoURL} />
+                
             </List.Item>
         )
     }
